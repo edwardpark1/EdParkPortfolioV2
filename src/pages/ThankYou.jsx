252b827679afc492c791bf652c15c20d505a1353
EdPark <edward.park010@gmail.com>
2023-09-17 17:53:43 -0400

    Removed form submission flag

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -5,19 +5,9 @@
     Version: 1.0
 */
 
-import { useEffect } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, } from "react-router-dom";
 
 export default function ThankYou() {
-    let navigate = useNavigate()
-    const { state } = useLocation();
-
-    useEffect(() => {
-        if (!state || (state.isFormSubmitted ?? false) !== true) {
-            navigate('/');
-        }
-    }, [navigate, state])
-
     return (
         <section className="w-[100%] mt-9 sm:mt-10 md:mt-11 flex flex-col flex-nowrap items-center justify-center gap-7 px-mobileBound sm:px-8 sm:gap-8 md:gap-9 lg:gap-10">
             <h1 className="text-center max-w-[18em]">Submission Received!</h1>
